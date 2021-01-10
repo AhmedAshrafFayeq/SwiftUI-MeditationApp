d2f3038410145badb833a5cec3586d6b862a626e
ahmedashraffayeq <ahmedashraffayeq@gmail.com>
2021-01-10 19:13:04 +0200

    first commit

---
+++ SwiftUI-MeditationApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-MeditationApp
+//
+//  Created by Ahmed Fayek on 1/10/21.
+//  Copyright © 2021 Ahmed Fayek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
